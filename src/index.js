fa9478c976390acd2463c4a07fb3fd3a654e0542
aliosman21 <amrali975@gmail.com>
2020-12-15 22:54:18 +0200

    added default styles to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./components/navbar";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 ReactDOM.render(
