3eb1ff3ff63ba7718c61e57cc8ae5173a3a2280e
aliosman21 <amrali975@gmail.com>
2020-12-17 17:36:29 +0200

    Handled changes in form component all in one function/handle click still needs to be done will link with backend and validate there/

--- src/components/form.js
+++ src/components/form.js
@@ -1,37 +1,77 @@
-import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import React from "react";
 
-class form extends Component {
+class form extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         name: "",
+         email: "",
+         pass: "",
+         address: "",
+      };
+   }
+   handleClick() {
+      console.log("this is:", this);
+   }
+   handleNameChange(e) {
+      this.setState((state) => {
+         return { [e.target.name]: e.target.value };
+      });
+      //console.log(this);
+      console.log(this);
+   }
+
    render() {
       return (
          <Form>
             <Form.Row>
                <Form.Label>Name</Form.Label>
-               <Form.Control type="text" placeholder="Enter Full Name" />
+               <Form.Control
+                  type="text"
+                  placeholder="Enter Full Name"
+                  name="name"
+                  onChange={(e) => {
+                     this.handleNameChange(e);
+                  }}
+               />
 
                <Form.Label>Email</Form.Label>
-               <Form.Control type="email" placeholder="Enter Email" />
+               <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  name="email"
+                  onChange={(e) => {
+                     this.handleNameChange(e);
+                  }}
+               />
             </Form.Row>
             <Form.Row>
                <Form.Label>Password</Form.Label>
-               <Form.Control type="password" placeholder="Password" />
+               <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="pass"
+                  onChange={(e) => {
+                     this.handleNameChange(e);
+                  }}
+               />
             </Form.Row>
 
             <Form.Row>
                <Form.Label>Address</Form.Label>
-               <Form.Control placeholder="1234 Main St" />
+               <Form.Control
+                  placeholder="1234 Main St"
+                  name="address"
+                  onChange={(e) => {
+                     this.handleNameChange(e);
+                  }}
+               />
             </Form.Row>
 
             <br></br>
-
-            <Button
-               variant="primary"
-               type="submit"
-               onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = "http://localhost:3000/";
-               }}>
+            <Button variant="primary" type="button" onClick={() => this.handleClick()}>
                Submit
             </Button>
          </Form>
